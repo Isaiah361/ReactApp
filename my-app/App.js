5ace63196e0347f92369637b3e397e9c5bf7782f
Isaiah Gonzalez <isaiah@Isaiahs-MacBook-Pro-2.local>
2019-03-08 23:13:30 -0500

    update

--- my-app/App.js
+++ my-app/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hey React is pretty cool hi</Text>
+        <Text>Hey React is pretty cool</Text>
       </View>
     );
   }
